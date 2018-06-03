7483f629fc0283ee19346b6e22fbb6b44f42d7db
Fırat KÜÇÜK <firatkucuk@gmail.com>
2018-06-03 10:04:08 +0200

    removed redundant secured annotations and transferred to spring boot 2

--- src/main/java/io/github/sandornemeth/HelloEndpoint.java
+++ src/main/java/io/github/sandornemeth/HelloEndpoint.java
@@ -1,6 +1,5 @@
 package io.github.sandornemeth;
 
-import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,13 +12,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloEndpoint {
 
     @GetMapping("/admin/hello")
-    @Secured("ROLE_ADMIN")
     public String sayHelloToAdmin() {
         return "Hello Admin";
     }
 
     @GetMapping("/user/hello")
-    @Secured("ROLE_USER")
     public String sayHelloToUser() {
         return "Hello User";
     }
